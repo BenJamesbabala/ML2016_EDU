749ada4406e9876664ab2ffb743aa4232a4037da
fnd212 <fnd@nyu.edu>
2016-04-15 21:44:58 -0400

    Up to first sparse repr

--- Cleaning/clean.py
+++ Cleaning/clean.py
@@ -1,5 +1,7 @@
 import pandas as pd 
 import numpy as np 
+from sklearn.feature_extraction import DictVectorizer
+
 
 
 def get_nulls_by_column(ds, filter='only_null'):
@@ -20,11 +22,11 @@ def valid_error_step_duration(ds):
     if type(ds) != pd.DataFrame:
         raise TypeError('ds must be pd.DataFrame')
 
-    columns = ['Error Step Duration (sec)','Correct First Attempt']
+    columns = ['error_step_duration','correct_first_attempt']
     ds_sub = ds[columns]
 
-    ESD_null = ds_sub[ds_sub['Error Step Duration (sec)'].isnull()]
-    ESD_missing_null = ESD_null[ESD_null['Correct First Attempt']==1]
+    ESD_null = ds_sub[ds_sub['error_step_duration'].isnull()]
+    ESD_missing_null = ESD_null[ESD_null['correct_first_attempt']==1]
 
     return ESD_missing_null.index
 
@@ -39,10 +41,9 @@ def set_value_for_index_column(ds, index, column, value):
 def fill_KC_null(ds, column):
     #Fill null values in KC column by the string 'null_unit'
     #taking unit value from ds['Problem Hierarchy']
-
     ds_na_KC = ds[ds[column].isnull()]
 
-    units = ds_na_KC['Problem Hierarchy']
+    units = ds_na_KC['unit']
     units_str = units.astype(str)
 
     fill_KC = pd.Series(['null_'+s for s in units_str.values],index=units_str.index)
@@ -52,17 +53,33 @@ def fill_KC_null(ds, column):
     return ds
 
 
+def fill_KC_op_null(ds, skill_column, opportunity_column):
+    #skill_column: str
+    #opportunity column: str
+    KCOP_null = ds[ds[opportunity_column].isnull()]
+
+    group = [skill_column, 'student_id']
+    KCOP_null.loc[(KCOP_null.index,opportunity_column)] = KCOP_null.groupby(group).cumcount()+1
+
+    KCOP_null[opportunity_column] = KCOP_null[opportunity_column].astype(str)
+
+    ds.loc[(KCOP_null.index,opportunity_column)] = KCOP_null[opportunity_column]
+    return ds
+    
+
+
+
 def unit_to_int(ds,test = False):
     #Replace unit strings to integers in a one-to-one mapping
 
-    units_str = ds['Problem Hierarchy'].unique()
+    units_str = ds['unit'].unique()
     units_int = range(len(units_str))
     mapping_dict = dict(zip(units_str,units_int))
 
     if test:       
-       return ds.replace({'Problem Hierarchy':mapping_dict}) , test.replace({'Problem Hierarchy':mapping_dict}) 
+       return ds.replace({'unit':mapping_dict}) , test.replace({'unit':mapping_dict}) 
 
-    return ds.replace({'Problem Hierarchy':mapping_dict})  
+    return ds.replace({'unit':mapping_dict})  
 
 
 def split_problem_hierarchy(ds):
@@ -77,19 +94,58 @@ def split_problem_hierarchy(ds):
     unit = pd.Series([u[0] for u in hierarchy.values],index=hierarchy.index)
     section = pd.Series([s[1] for s in hierarchy.values],index=hierarchy.index)
 
-    ds['Unit'] = unit
-    ds['Section'] = section
+    ds['unit'] = unit
+    ds['section'] = section
 
     return ds
 
 def create_unique_step_id(ds):
-    grouped = train.groupby(['Unit','Section','Problem Name', 'Step Name'])
+    grouped = train.groupby(['unit','section','problem_name', 'step_name'])
     groups = grouped.groups
+    unique_step_id = range(len(groups))
     #groups is a dictionary
 
+
 def renamer(data_frame):
-    data_renamed = data_frame.rename(columns={'Row': 'row', 'Anon Student Id': 'student_id', 'Problem Name': 'problem','Problem View': 'view', 'Step Name': 'step_name', 'Step Start Time': 'start_time', 'First Transaction Time': 'first_trans_time', 'Correct Transaction Time': 'correct_trans_time', 'Step End Time': 'end_time', 'Step Duration (sec)': 'step_duration', 'Correct Step Duration (sec)':'correct_step_duration', 'Error Step Duration (sec)':'error_step_duration','Correct First Attempt':'correct_first_attempt', 'Incorrects':'incorrects', 'Hints':'hints', 'Corrects':'corrects','KC(SubSkills)':'kc_subskills', 'Opportunity(SubSkills)':'opp_subskills', 'KC(KTracedSkills)':'k_traced_skills','Opportunity(KTracedSkills)':'opp_k_traced', 'KC(Rules)': 'kc_rules', 'Opportunity(Rules)': 'opp_rules','Unit':'unit', 'Section':'section'})
+    data_renamed = data_frame.rename(columns={'Row': 'row', 'Anon Student Id': 'student_id',
+                                            'Problem Name': 'problem_name','Problem View': 'view', 'Step Name': 'step_name',
+                                            'Step Start Time': 'start_time', 'First Transaction Time': 'first_trans_time',
+                                            'Correct Transaction Time': 'correct_trans_time', 'Step End Time': 'end_time',
+                                            'Step Duration (sec)': 'step_duration', 'Correct Step Duration (sec)':'correct_step_duration',
+                                            'Error Step Duration (sec)':'error_step_duration','Correct First Attempt':'correct_first_attempt',
+                                            'Incorrects':'incorrects', 'Hints':'hints', 'Corrects':'corrects',
+                                            'KC(SubSkills)':'kc_subskills', 'Opportunity(SubSkills)':'opp_subskills',
+                                            'KC(KTracedSkills)':'k_traced_skills', 'Opportunity(KTracedSkills)':'opp_k_traced', 
+                                            'KC(Rules)': 'kc_rules', 'Opportunity(Rules)': 'opp_rules'})
     return data_renamed
+
+
+
+
+def sparse_kc_skills(ds, skill_column, opportunity_column):
+
+    #Create temporal columns
+    ds['KCop'] = ds[opportunity_column].str.split('~~')
+    ds['KC'] = ds[skill_column].str.split('~~')
+    ds['KCzip'] = map(zip,ds.KC,ds.KCop)
+    ds['KCdict'] = ds.KCzip.map(dict)
+
+    #Create sparse matrix
+    #sparse_ds = pd.DataFrame(list(ds['KCdict']), index = ds.index)
+    list_dicts = list(ds['KCdict'])
+    v = DictVectorizer(sparse=True)
+    sparse_ds = v.fit_Transform(list_dicts)
+    
+    #Remove temporal columns
+    ds.drop('KCop',1)
+    ds.drop('KC',1)
+    ds.drop('KCzip',1)
+    ds.drop('KCdict',1)
+
+    return sparse_ds
+
+
+
 
 def main():
     
@@ -99,12 +155,26 @@ def main():
     #Dataset contains a column called Error Step Duration which can be NaN
     #if there is a missing value or if the step was solved correctly (valid NaN). 
     #Set the value of valid NaNs to -1
-    set_value_for_index_column(train, valid_error_step_duration(train), 'Error Step Duration (sec)',-1)
-    train = split_problem_hierarchy(train)
-
-    train = unit_to_int(train)
-    train = fill_KC_null(train, 'KC(SubSkills)')
     train = renamer(train)
+    set_value_for_index_column(train, valid_error_step_duration(train), 'error_step_duration',-1)    
+    train = split_problem_hierarchy(train)
+    train = unit_to_int(train)
+
+    train = fill_KC_null(train, 'kc_subskills')
+    train = fill_KC_null(train, 'k_traced_skills')
+    train = fill_KC_null(train, 'kc_rules')
+
+    train = fill_KC_op_null(train, 'kc_subskills', 'opp_subskills')
+    train = fill_KC_op_null(train, 'k_traced_skills', 'opp_k_traced')
+    train = fill_KC_op_null(train, 'kc_rules', 'opp_rules')
+
+    subskills_sparse = sparse_kc_skills(train, 'kc_subskills','opp_subskills')
+    subskills_sparse = sparse_kc_skills(train, 'k_traced_skills','opp_k_traced')
+    subskills_sparse = sparse_kc_skills(train, 'kc_rules','opp_rules')
+
+
+
+    
 
 
 
@@ -114,3 +184,7 @@ if __name__ == '__main__':
 
 
 
+subtrain[subtrain.opp_subskills.isnull()].groupby(['kc_subskills','student_id'])
+
+
+train['cuenta'] = train[train.opp_subskills.isnull()].groupby(['kc_subskills','student_id']).cumcount()+1
