26e07eba17a7e7a8cc9c2b62df6c7917bdefd059
fnd212 <fnd@nyu.edu>
2016-04-25 14:23:05 -0400

    baseline model updated, split into train and test, train and evaluate

--- baselineModel/baseline.py
+++ baselineModel/baseline.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-%matplotlib inline
 
 
 data = pd.read_csv('/home/michael/Desktop/machineLearningProject/algebra_2008_2009/algebra_2008_2009_train.txt' , delimiter ='\t')
@@ -21,32 +20,87 @@ problemNames = list(cleanData['fullStepName'].values)
 
 
 def sigmoid(x):
-    return 1.0 / (1.0 + np.exp(-x))
+    return ( 1.0 / (1.0 + np.exp(-x)) )
 
 
 def objectiveFunction(df,theta,beta):
 	'''
-	theta must be a series with the studentId as key
-	beta must be a series with problemName as key
+	theta must be a pd.Series with the studentId as key
+	beta must be a pd.Series with problemName as key
 	returns float
 	iterates over every row in df
 
 	'''
    
     total = 0
-    
-    
+        
     studentPriorMean = 0
     studentPriorStd = 1
     problemPriorMean = 0
     problemPriorStd = 1
+
+
+    priorStudent = -(theta[studentId] - studentPriorMean )**2/(2*studentPriorStd**2)
+
     
-    for row in df.iterrows():        
+    for row in df.iterrows():  
+
         studentId =  row[1][0]
         correct = row[1][2]
         problemName = row[1][1]
         
+        priorStudent = -(theta[studentId] - studentPriorMean )**2/(2*studentPriorStd**2)
+        priorProblem = -(beta[problemName] - problemPriorMean )**2/(2*problemPriorStd**2)
         
+        
+        if correct:
+            joint = np.log( sigmoid(  theta[studentId] - beta[problemName]  ) )
+        else:
+            joint = np.log(1  - sigmoid( theta[studentId] - beta[problemName] ))
+        
+        total += priorStudent + priorProblem + joint
+    
+    return total
+            
+
+
+
+
+def objectiveFunction(df,theta,beta):
+    '''
+    theta must be a pd.Series with the studentId as key
+    beta must be a pd.Series with problemName as key
+    returns float
+    iterates over every row in df
+
+    '''
+   
+    total = 0
+        
+    studentPriorMean =  
+    studentPriorStd = 1
+    problemPriorMean = 0
+    problemPriorStd = 1
+
+
+    priorStudent = -(theta[studentId] - studentPriorMean )**2/(2*studentPriorStd**2)
+
+
+    for student in df.student_id.unique:
+    #loop over each different student
+        ds_student = ds[ds.student_id == student]
+        student_problems = ds_student.problem_id.unique()
+
+        
+
+
+
+    
+    for row in df.iterrows():  
+          
+        studentId =  row[1][0]
+        correct = row[1][2]
+        problemName = row[1][1]
         
         priorStudent = -(theta[studentId] - studentPriorMean )**2/(2*studentPriorStd**2)
         priorProblem = -(beta[problemName] - problemPriorMean )**2/(2*problemPriorStd**2)
--- baselineModel/baseline_lr.py
+++ baselineModel/baseline_lr.py
@@ -2,8 +2,10 @@ import numpy as np
 import pandas as pd
 from scipy.sparse import hstack, coo_matrix, vstack
 import scipy
+import sklearn
 from sklearn.linear_model import LogisticRegression
 from sklearn.linear_model import LogisticRegressionCV
+from sklearn.metrics import mean_squared_error, log_loss, accuracy
 
 
 
@@ -13,7 +15,7 @@ def remove_unused_columns(ds):
     unused_columns = [u'problem_name', u'view', u'step_name',
        u'start_time', u'first_trans_time', u'correct_trans_time', u'end_time',
        u'step_duration', u'correct_step_duration', u'error_step_duration',
-       u'correct_first_attempt', u'incorrects', u'hints', u'corrects',
+       u'incorrects', u'hints', u'corrects',
        u'kc_subskills', u'opp_subskills', u'k_traced_skills', u'opp_k_traced',
        u'kc_rules', u'opp_rules', u'unit', u'section', u'problem_id']
 
@@ -44,24 +46,63 @@ def create_dummy_representation(ds):
 
 
 def main():
+    
+    train_ix, test_ix = splitter(train)
+
+    train_lr = train.ix[train_ix].copy()
+    test_lr = train.ix[test_ix].copy()
 
     #Only keep the necessary features for the baseline model
-    train_lr = remove_unused_columns(train)
+    train_lr = remove_unused_columns(train_lr)
+    test_lr = remove_unused_columns(test_lr)
 
-    #Create sparse X matrix
-    X_steps = create_sparse_occurences(train_lr, 'step_id')
-    X_stud = create_sparse_occurences(train_lr, 'student_id')
-    X = hstack((X_stud, X_steps))
+    reset_index(train_lr)
+    reset_index(test_lr)
 
-    y = train_lr.y_one_negative_one
 
-    #Grid of 10 for regularization in cross validation
-    Cs = np.linspace(1e-4, 1e2, 10)
+    #Train set
+    #Create sparse X_train matrix
+    X_train_steps = create_sparse_occurences(train_lr, 'step_id')
+    X_train_stud = create_sparse_occurences(train_lr, 'student_id')
+    X_train = hstack((X_train_stud, X_train_steps))
+
+    y_train = train_lr.y_one_negative_one
+    y01_train = train_lr.correct_first_attempt
+
+    #Test set
+    X_test_steps = create_sparse_occurences(test_lr, 'step_id')
+    X_test_stud = create_sparse_occurences(test_lr, 'student_id')
+    X_test = hstack((X_test_stud, X_test_steps))    
+
+    y_test = train_lr.y_one_negative_one
+    y01_test = train_lr.correct_first_attempt
+    
+
+    #Grid of N for regularization in cross validation
+    N = 15
+    Cs = np.linspace(1e-4, 1e2, N)
     lr = LogisticRegressionCV(Cs = Cs, fit_intercept=True, penalty='l2', 
         scoring='log_loss', n_jobs=3)
-    lr.fit(X,y)
+    lr.fit(X_train, y_train)
+
+    #Evaluation in train set
+    pred_proba_train = lr.predict_proba(X_train)
+    pred_proba_train_1 = [x[1] for x in pred_proba_train]
+    pred_class_train = lr.predict(X_train)
+
+    mse_train = mean_squared_error(y_01_train, pred_proba_train_1)
+    rmse_train = np.sqrt(mse_train)
+    logloss_train = log_loss(y_01_train, pred_proba_train_1)
 
 
+    #Evaluation in test set
+    pred_proba_test = lr.predict_proba(X_test)
+    pred_proba_test_1 = [x[1] for x in pred_proba_test]
+    pred_class_test = lr.predict(X_test)
+
+    mse_test = mean_squared_error(y_01_test, pred_proba_test_1)
+    rmse_test = np.sqrt(mse_test)
+    logloss_test = log_loss(y_01_test, pred_proba_test_1)
 
 
 if __name__ == '__main__':
