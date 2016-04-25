3529c984fbafa616153c64f9dcb7e88bd2967571
fnd212 <fnd@nyu.edu>
2016-04-25 14:05:25 -0400

    updated splitter function

--- Cleaning/splitter.py
+++ Cleaning/splitter.py
@@ -3,8 +3,8 @@ import pandas as pd
 def splitter(data_frame):
     #data_frame: pandas dataframe
 
-    test_indexes = list() 
-    ds_grouped = data_frame.groupby(['Anon Student Id', 'Unit'])
+    test_indices = list() 
+    ds_grouped = data_frame.groupby(['student_id', 'unit'])
     indices = ds_grouped.indices
 
     for student, unit in indices:
@@ -17,10 +17,14 @@ def splitter(data_frame):
             else:
 
                 data_unit_student = data_frame.ix[indices[student,unit]]
-                problem_name = data_unit_student['Problem Name'][data_unit_student.index[-1]]
-                data_last_problem = data_unit_student[data_unit_student['Problem Name']==problem_name]
-                last_view = data_last_problem['Problem View'][data_last_problem.index[-1]]
-                data_last_view = data_last_problem[data_last_problem['Problem View']==last_view]
-                test_indexes.extend(data_last_view.index)
+                problem_name = data_unit_student['problem_id'][data_unit_student.index[-1]]
+                data_last_problem = data_unit_student[data_unit_student['problem_id']==problem_name]
+                last_view = data_last_problem['view'][data_last_problem.index[-1]]
+                data_last_view = data_last_problem[data_last_problem['view']==last_view]
+                test_indices.extend(data_last_view.index)
 
-    return test_indexes
+    all_indices = list(data_frame.index)
+    train_indices = list(set(all_indices)-set(test_indices))
+
+
+    return train_indices, test_indices
