e704117541ae9e19d396e6b628e5b4a06e06a00f
fnd212 <fnd212@nyu.edu>
2016-04-30 11:10:12 -0400

    removing old names

--- FeatureCreation/features.py
+++ FeatureCreation/features.py
@@ -29,35 +29,8 @@ def skills_knowledge_counter(df, features, sparse_matrix_input, subskills_col):
 
 
 
-def skills_corr_counter(ds, sparse_matrix_input):
-    # SHIFT EVERYTHING BY ONE NOT TO LEAK!!!!!!!!
-    #New Version: 
-    # Timing:   Wed Apr 27 02:21:00 2016
-    #           Wed Apr 27 02:25:35 2016
-    student_cfa = ds[['student_id', 'correct_first_attempt']]
-    sparse_matrix = csr_matrix(sparse_matrix_input.shape)
-
-    for col in xrange(sparse_matrix_input.shape[1]):
-
-        skill_indices = np.array(sparse_matrix_input[:,col].nonzero()[0])
-
-        s_cfa = student_cfa.ix[skill_indices]
-        sg = s_cfa.groupby('student_id').cumsum()
-        indices = np.array(sg.index)
-        values = sg.values
-
-        sparse_matrix[indices,col] = values
-
-    return sparse_matrix
-
-
-
-
-def skills_corr_counter_win(ds, sparse_matrix_input):
-
-    #New Version: 
-    # Timing:   Wed Apr 27 02:21:00 2016
-    #           Wed Apr 27 02:25:35 2016
+def skills_corr_counter_win(ds, sparse_matrix_input, window=None):
+    #If window not specified not use window
     student_cfa = ds[['student_id', 'correct_first_attempt']]
     sparse_matrix = csr_matrix(sparse_matrix_input.shape)
 
@@ -67,9 +40,12 @@ def skills_corr_counter_win(ds, sparse_matrix_input):
 
         s_cfa = student_cfa.ix[skill_indices]
         grouped = s_cfa.groupby('student_id')
-        sg = grouped.apply(cumsum_window)
-        sg = sg.reset_index(level=0).drop('student_id',axis=1)
 
+        if window:
+            sg = grouped.apply(cumsum_window, window)
+            sg = sg.reset_index(level=0).drop('student_id',axis=1)
+        else:
+            sg = grouped.cumsum()
         
         indices = np.array(sg.index)
         values = sg.values
@@ -77,29 +53,6 @@ def skills_corr_counter_win(ds, sparse_matrix_input):
         sparse_matrix[indices,col] = values
 
     return sparse_matrix
-
-
-6277028    stu_ffe1e527a8
-6282419    stu_ffe1e527a8
-6286301    stu_ffe1e527a8
-6293695    stu_ffe1e527a8
-6304565    stu_ffe1e527a8
-6307077    stu_ffe1e527a8
-6334143    stu_ffe1e527a8
-6337164    stu_ffe1e527a8
-6407736
-
-[6277028, 
-6282419,
-6286301,
-6293695,
-6304565,
-6307077,
-6334143,
-6337164,
-6407736]
-
-
 
 def cumsum_window(obs, N=5):
     cum = obs.cumsum().correct_first_attempt
@@ -114,41 +67,21 @@ def cumsum_window(obs, N=5):
     
     return diff
 
-    
 
+def create_and_save_sparses(ds, sparse_list, window_list):
 
+    sp_n = 0
 
-########################################
-#   OLD VERSION
-########################################3
-# def skills_corr_counter(ds, sparse_matrix_input):
+    for sp in sparse_list:
+        sp_n = sp_n+1
+        for window in window_list:
+            sparse_cum = skills_corr_counter_win(ds,
+                                sp, window=window)
 
-#     #This was the second version. The part that is still too slow is the filtering and groupby of the ds
-#     students = ds.student_id.unique()
-#     student_cfa = ds[['student_id', 'correct_first_attempt']]
-#     students_group = student_cfa.groupby('student_id')
-#     groups = students_group.groups
-#     sparse_matrix = csr_matrix(sparse_matrix_input.shape)
+            np.save('matrix_'+str(sp_n)+'_win_'+str(window)+'.npy',
+                    sparse_cum) 
+        print ('Matrix number {} window {} completed'.format(sp_n, window))
 
-#     for col in xrange(sparse_matrix_input.shape[1]):
-
-#         #new_column = pd.Series(np.zeros(ds.shape[0]))
-
-#         skill_indices = np.array(sparse_matrix_input[:,col].nonzero()[0])
-
-#         for student in students:
-#             student_index = np.array(groups[student])
-#             indices = np.intersect1d(skill_indices, student_index, assume_unique=True)
-            
-#             cumsum = np.array(student_cfa.ix[indices].correct_first_attempt.cumsum())
-#             cumsum = cumsum.reshape(len(cumsum),1)
-#             sparse_matrix[indices,col] = cumsum
-
-
-#             #new_column.loc[indexes] = np.array(student_cfa.correct_first_attempt.cumsum())
-#             #new_column.loc[indexes] = ds.ix[indexes].groupby('student_id').cumsum().correct_first_attempt.reshape((indexes.shape[0], 1))
-#     return sparse_matrix
-
-print time.ctime()
-subskills_count_win = skills_corr_counter_win(ds, subskills_sparse)
-print time.ctime()
+#sparse_list = [subskills_sparse, k_traced_sparse, kc_rules_sparse]
+#windows = [1,2,3,4,5,6,7,8,9,10]
+# create_and_save_sparses(train, sparse_list, windows)
