9db3928d9dd40b475498e2838df3edd11ceda02a
fnd212 <fnd212@nyu.edu>
2016-05-14 00:07:30 -0400

    init file updated

--- __init__.py
+++ __init__.py
@@ -1,6 +1,13 @@
+import xgboost as xgb
+
+
 from Cleaning.clean import *
 from Cleaning.splitter import *
 from skillsClustering.ClusterSkills import *
-from FeatureCreation.features import *
 from Models.models import *
-from recEngine.LatentVariables import *
+from Models.baseline_lr  import *
+from FeatureCreation.features import *
+from Models.model_calibration import *
+from Models.xgboost_models import *
+from Graphics.graphics import *
+from recEngine.make_clusters import *
