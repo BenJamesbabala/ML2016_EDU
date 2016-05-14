c3dcae9b90b7f713bfb0287e96a26bbdc71d83a7
fnd212 <fnd212@nyu.edu>
2016-05-13 23:34:57 -0400

    stable xgboost config

--- Models/xgboost_models.py
+++ Models/xgboost_models.py
@@ -5,14 +5,14 @@ import numpy as np
 #dval = xgb.DMatrix(X_val, y_val)
 #
 #dxtest = xgb.DMatrix( X_test)
-#
-#param = { 'booster':'gbtree','eval_metric':'logloss', 'silent':0, 
-#        'tree_method':'exact','bst:alpha':1e-2, 'bst:eta':0.1, 
-#        'bst:scale_pos_weight':0.171158, 'max_depth':1, 
-#        'nthread':20}
+##'scale_pos_weight':0.171158,
+##'eval_metric':'logloss'
+#param = { 'booster':'gbtree', 'silent':0, 
+#        'tree_method':'exact','lambda':10e-3, 'eta':0.1, 
+#         'max_depth':50, 'objective':'binary:logitraw'}
 #
 #evallist  = [(dval,'eval'), (dtrain,'train')]
-#num_rounds = 10
+#num_rounds = 100
 #bst = xgb.train( param, dtrain, num_rounds, evallist )
 #
 #
@@ -21,60 +21,31 @@ import numpy as np
 #dval = xgb.DMatrix(X_val, y_val)
 #
 #train_rmse, train_ll, val_rmse, val_ll = gridsearch_xgboost(dtrain, dval, y_train, y_val)
-
-
-
-
-#bst = xgb.XGBRegressor(max_depth=3, learning_rate=0.1, 
-#                n_estimators=1000, silent=False, 
-#                objective='binary:logistic', nthread=-1, 
-#                gamma=0, min_child_weight=1, max_delta_step=0, 
-#                subsample=1, colsample_bytree=1, colsample_bylevel=1, 
-#                reg_alpha=10e-2, reg_lambda=1, scale_pos_weight=1, 
-#                base_score=0.171158, seed=0, missing=None)
 #
-#bst.fit(X_train, y_train, eval_metric='logloss')
-
-#param = { 'booster':'gbtree','eval_metric':'logloss', 
-#        'tree_method':'exact' }
-
+#
+#param = {'bst:max_depth':2, 'bst:eta':1, 'silent':1, 'objective':'binary:logitraw' }
+#param['nthread'] = 20
 #param['eval_metric'] = ['logloss']
 #param['tree_method'] = 'exact'
-
+#
 #evallist  = [(dval,'eval'), (dtrain,'train')]
-
-#bst = xgb.train(params=param, dtrain=dtrain, num_boost_round=100, 
-#            evals=evallis)
-
+#
+#num_round = 200
 #bst = xgb.train( param, dtrain, num_round, evallist )
 
 
-
-
-
-#pred_proba_train = bst.predict(X_train)
-#
-#mse_train = mean_squared_error(y_train, pred_proba_train)
-#rmse_train = np.sqrt(mse_train)
-#logloss_train = log_loss(y_train, pred_proba_train)
-#
-##Evaluation in validation set
-#pred_proba_val = bst.predict(X_val)
-#
-#mse_val = mean_squared_error(y_val, pred_proba_val)
-#rmse_val = np.sqrt(mse_val)
-#logloss_val = log_loss(y_val, pred_proba_val)
-#
-#rmse_train
-#rmse_val
-#logloss_train
-#logloss_val
-
+########################
+# XGBOOST GRIDSEARCH
+########################
 
 def gridsearch_xgboost(dtrain, dval, y_train, y_val):
     
-    depths = [3,20,50,100]
-    alphas = np.logspace(-7,0,7)
+    depths = [3,10,50]
+    
+    depth=2
+    min_child_weight = 10
+    alphas = np.logspace(-7,1,4)
+    colsample = 0.5
 
     train_rmse = {}
     train_ll = {}
@@ -85,12 +56,16 @@ def gridsearch_xgboost(dtrain, dval, y_train, y_val):
         for alpha in alphas:
 
             param = { 'booster':'gbtree','eval_metric':'logloss', 'silent':0, 
-                    'tree_method':'exact','bst:alpha':alpha, 'bst:eta':0.1, 
-                    'bst:scale_pos_weight':0.171158, 'max_depth':depth, 
-                    'nthread':20}
+                    'tree_method':'exact','lambda':alpha, 'eta':0.1, 
+                    'max_depth':depth, 'objective':'binary:logistic', 
+                    'min_child_weight':min_child_weight, 
+                    'colsample_bytree':colsample }
+                    #'n_estimators':10000}
+                    #
+                    #'nthread':1}
             
             evallist  = [(dval,'eval'), (dtrain,'train')]
-            num_rounds = 10
+            num_rounds = 500
             bst = xgb.train( param, dtrain, num_rounds, evallist )
         
             pred_proba_train = bst.predict(dtrain)
@@ -114,43 +89,46 @@ def gridsearch_xgboost(dtrain, dval, y_train, y_val):
     return train_rmse, train_ll, val_rmse, val_ll
 
 
-"""
----------------------------------------------------
-import numpy as np
-X_train = np.load('X_train.npy').item()
-X_test = np.load('X_test.npy').item()
-y_test = np.load('y_test.npy')
-y_train = np.load('y_train.npy')
-
-----------------------------------------------
-#Evaluation
-
-First Model
-
-[299]   eval-logloss:0.369825   train-logloss:0.356933
-[299]   eval-rmse:0.336063  train-rmse:0.331482
 
 
-Second Model (Super X Small)
+def main():
 
-[299]   eval-rmse:0.318282  train-rmse:0.317684
+    ########################
+    # XGBOOST NORMAL FIT
+    ########################
+
+    depth=5
+    min_child_weight = 10
+    alphas = np.logspace(-7,1,4)
+    colsample = 0.5
+
+    param = { 'booster':'gbtree','eval_metric':'logloss', 'silent':0, 
+            'tree_method':'exact','lambda':alpha, 'eta':0.1, 
+            'max_depth':depth, 'objective':'binary:logistic', 
+            'min_child_weight':min_child_weight, 
+            'colsample_bytree':colsample }
+            #'n_estimators':10000}
+            #
+            #'nthread':1}
+    
+    evallist  = [(dval,'eval'), (dtrain,'train')]
+    num_rounds = 500
+    bst = xgb.train( param, dtrain, num_rounds, evallist )
+    
+    pred_proba_train = bst.predict(dtrain)
+    
+    mse_train = mean_squared_error(y_train, pred_proba_train)
+    rmse_train = np.sqrt(mse_train)
+    logloss_train = log_loss(y_train, pred_proba_train)
+    
+    #Evaluation in validation set
+    pred_proba_val = bst.predict(dval)
+    
+    mse_val = mean_squared_error(y_val, pred_proba_val)
+    rmse_val = np.sqrt(mse_val)
+    logloss_val = log_loss(y_val, pred_proba_val)
 
 
-Third Model (Super X Small 6000 iterations)
 
-[1930]  eval-rmse:0.312692  train-rmse:0.304757
-
--------------------------------------------------------
-#Plotting 
-
-fraction_of_positives, mean_predicted_value = calibration_curve(y_test2, ypred, normalize=False, n_bins=50)
-plt.plot([0,1],[0,1], 'k:', label='Perfectly Calibrated')
-plt.plot(mean_predicted_value, fraction_of_positives, "s-", label="Actual")
-plt.ylabel("Fraction of positives")
-ax1.legend(loc="lower right")
-plt.title('Calibration plots  (reliability curve)')
-
-
-"""
-
-
+if __name__ == '__main__':
+    main()
