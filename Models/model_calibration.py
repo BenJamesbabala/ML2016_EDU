8559b1ffe8641f64f29d43781c988084c6bfe2a4
fnd212 <fnd212@nyu.edu>
2016-05-13 23:28:16 -0400

    Model calibration function added

---
+++ Models/model_calibration.py
@@ -0,0 +1,17 @@
+from sklearn.calibration import CalibratedClassifierCV
+
+
+def calibrate(X_val, y_val, estimator):
+
+    clf = CalibratedClassifierCV(base_estimator=estimator, 
+                                method='sigmoid', cv='prefit')
+
+    clf.fit(X_val, y_val)
+    return clf
+
+def main():
+    pass    
+
+
+if __name__ == '__main__':
+    main()
